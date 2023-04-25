2a5d5a61c5e004d46a2b4046dc199bacd56f95ba
gogoyooni <taeyoondev@gmail.com>
2023-04-25 11:43:55 +0900

    add progress bar when route changes - using route event of next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,10 +8,19 @@ import { ThemeProvider, ChakraProvider, CSSReset,theme } from "@chakra-ui/react"
 import Layout from '../../components/Layout';
 
 export default function App({ Component, pageProps }) {
+  NProgress.configure({showSpinner: false})
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  })
   return (
     <>
       <Head>
-
+  
       </Head>
       <ChakraProvider>
         <Layout>
